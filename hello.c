899cd51de7a9ef58824e5a16c31fa9158a9d1bef
jayjay <jayjaygaver@gmail.com>
2023-08-24 17:47:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jay Jay Gaver
+ * Partner: John
+ *
+ * Date: 2023/08/21
+ *
+ * prints out name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+ printf("Jay Jay\n");
+ return 0;
+}
